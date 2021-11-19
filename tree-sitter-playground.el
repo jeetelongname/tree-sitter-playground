37470df8ff9fcda6cf0396f88d336085a7355ca4
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-11-19 21:30:47 +0000

    inital commit

---
+++ tree-sitter-playground.el
@@ -0,0 +1,11 @@
+;;; tree-sitter-playground.el --- Better tree visualisation for tree sitter  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Jeetaditya Chatterjee
+
+;; Author: Jeetaditya Chatterjee <jeetelongname@gmail.com>
+;; Keywords: languages, tools,
+;;; Commentary:
+;;; Code:
+
+(provide 'tree-sitter-playground)
+;;; tree-sitter-playground.el ends here
